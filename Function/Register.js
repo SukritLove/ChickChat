86bf599118092467a7a0b5d9f97c2defa8d60683
Sukrit Rerk-u-cote <sukrit_love@Sukrits-MacBook-Pro.local>
2023-10-21 12:02:08 +0700

    Validate Frm DB [Not Finish]

--- Function/Register.js
+++ Function/Register.js
@@ -1,45 +1,101 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const registerButton = document.getElementById("register");
-
-    registerButton.addEventListener("click", function () {
-        // Retrieve form values
-        const username = document.getElementById("username").value;
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
-        const rePassword = document.getElementById("re-pass").value;
-
-        // You can perform client-side validation here
-        if (username === "") {
-            alert("Username is required.");
-            return;
-        }
-
-        if (email === "") {
-            alert("Email is required.");
-            return;
-        }
-
-        if (password === "") {
-            alert("Password is required.");
-            return;
-        }
-
-        if (password !== rePassword) {
-            alert("Passwords do not match.");
-            return;
-        }
-
-        // If all validation passes, you can send the data to PHP using AJAX
-        const xhr = new XMLHttpRequest();
-        xhr.open("POST", "../php/Register_User.php", true);
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                // Handle the response from PHP
-                alert(xhr.responseText);
-            }
-        };
-        const data = `username=${username}&email=${email}&password=${password}`;
-        xhr.send(data);
-    });
+  const registerButton = document.getElementById("register");
+  registerButton.addEventListener("click", function () {
+    validateData();
+  });
 });
+
+async function callPhpMethod(method, column, data) {
+  console.log("Use PHP");
+  try {
+    const response = await fetch("../php/Register_User.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `action=${method}&column=${column}&data=${data}`,
+    });
+    const result = await response.text();
+
+    if (result === "false" || result === "true") {
+      console.log("Done");
+    } else {
+      console.error("Unexpected response:" + result);
+    }
+
+    console.log("Use PHP result : " + result);
+    return result;
+  } catch (error) {
+    console.error("Error:", error);
+    return null;
+  }
+}
+
+async function checkUserName(func) {
+  const username = document.getElementById("username").value;
+  console.log("Check User");
+  if (func === 1) {
+    if (username === "") {
+      return false;
+    }
+  } else {
+    const result = await callPhpMethod("Checker", "username", username);
+    return result;
+  }
+}
+
+function validateData() {
+  // Retrieve form values
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const rePassword = document.getElementById("re-pass").value;
+
+  var errorSet = [
+    document.getElementById("erroruser"),
+    document.getElementById("erroremail"),
+    document.getElementById("errorpass"),
+    document.getElementById("errorrepass"),
+  ];
+
+  checkUserName(1).then((isUsernameValid) => {
+    errorSet[0].style.visibility = isUsernameValid ? "hidden" : "visible";
+    errorSet[0].textContent = "Username missing! Let's fix that.";
+    return;
+  });
+  checkUserName(2).then((isUsernameValid) => {
+    errorSet[0].style.visibility = isUsernameValid ? "hidden" : "visible";
+    errorSet[0].textContent = "This Username is already been use!";
+    return;
+  });
+  // You can perform client-side validation here
+
+  // if (email === "") {
+  //   alert("Email is required.");
+  //   return;
+  // }
+
+  // if (password === "") {
+  //   alert("Password is required.");
+  //   return;
+  // }
+
+  // if (password !== rePassword) {
+  //   alert("Passwords do not match.");
+  //   return;
+  // }
+}
+
+function SendPHP() {
+  // // If all validation passes, you can send the data to PHP using AJAX
+  // const xhr = new XMLHttpRequest();
+  // xhr.open("POST", "../php/Register_User.php", true);
+  // xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  // xhr.onreadystatechange = function () {
+  //     if (xhr.readyState === 4 && xhr.status === 200) {
+  //         // Handle the response from PHP
+  //         alert(xhr.responseText);
+  //     }
+  // };
+  // const data = `username=${username}&email=${email}&password=${password}`;
+  // xhr.send(data);
+}
--- PHP/Register_User.php
+++ PHP/Register_User.php
@@ -1,44 +1,54 @@
 <?php
-try {
-    // Database configuration
-    $host = 'localhost';
-    $port = 3306;
-    $dbname = 'ChickChatData';
-    $db_username = 'Sukrit'; // Use a different variable name for the database username
-    $db_password = 'admin435123'; // Use a different variable name for the database password
 
-    // User registration data from POST request
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $user_password = $_POST['password']; // Use a different variable name for the user password
+$host = 'localhost';
+$port = 3306;
+$dbname = 'ChickChatData';
+$db_username = 'Sukrit';
+$db_password = 'admin435123';
 
-    // Create a new PDO instance for database connection
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $db_username, $db_password);
+// Function to establish a database connection
+function connectToDatabase()
+{
+    global $host, $port, $dbname, $db_username, $db_password;
 
-    // Set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn = new mysqli($host, $db_username, $db_password, $dbname);
 
-    // Hash the user's password before inserting it into the database (use a strong hashing algorithm like bcrypt)
-    $hashedPassword = password_hash($user_password, PASSWORD_BCRYPT);
-
-    // Insert user data into the database
-    $query = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password)";
-    $stmt = $pdo->prepare($query);
-
-    // Bind parameters
-    $stmt->bindParam(':username', $username, PDO::PARAM_STR);
-    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-    $stmt->bindParam(':password', $hashedPassword, PDO::PARAM_STR);
-
-    // Execute the query
-    $stmt->execute();
-
-    // Close the database connection
-    $pdo = null;
-
-    echo "Registration successful! Username: $username, Email: $email";
-} catch (PDOException $e) {
-    // Handle database connection errors
-    echo "Database error: " . $e->getMessage();
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    return $conn;
 }
-?>
+
+
+function Checker($column, $data)
+{
+    $conn = connectToDatabase();
+
+    $table = 'users';
+
+    $data = $conn->real_escape_string($data);
+
+    $query = "SELECT * FROM $table WHERE $column = '$data'";
+    $result = $conn->query($query);
+
+    echo $result->num_rows > 0 ? "false" : "true";
+}
+
+function method2($data)
+{
+    // Your PHP code for method 2
+    return "Method 2 called with data: " . $data;
+}
+
+// Check which method to call based on the "action" parameter
+if (isset($_POST['action'])) {
+    $action = $_POST['action'];
+    $column = $_POST['column'];
+    $data = $_POST['data'];
+
+    if ($action === 'Checker') {
+        echo Checker($column, $data);
+    } elseif ($action === 'method2') {
+        echo method2($data);
+    }
+}
